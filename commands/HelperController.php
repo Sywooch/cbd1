20c341ae5aaccb16aa993fffeefdc217a1758797
mxuser <mxuser@ya.ru>
2018-01-03 16:50:40 +0200

    bugfix

--- commands/HelperController.php
+++ commands/HelperController.php
@@ -15,22 +15,43 @@ use \yii\console\Controller;
 class HelperController extends Controller
 {
 
+    public function actionCleardb(){
+        Yii::$app->db->createCommand('delete from user where username not in ("organizator", "neiron", "member", "member1")')->execute();
+        Yii::$app->db->createCommand('delete from api_organizations where user_id is null or user_id not in(select id from user)')->execute();
+        Yii::$app->db->createCommand('delete from api_identifiers where id not in (select identifier_id from api_organizations)')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_auctions')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_awards')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_award_organizations')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_bids')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_cancellations')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_contracts')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_documents')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_items')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_items_classifications')->execute();
+        Yii::$app->db->createCommand('TRUNCATE api_questions')->execute();
+        Yii::$app->db->createCommand('TRUNCATE email_tasks')->execute();
+        Yii::$app->db->createCommand('TRUNCATE files')->execute();
+        Yii::$app->db->createCommand('TRUNCATE lots')->execute();
+        Yii::$app->db->createCommand('TRUNCATE messages')->execute();
+        Yii::$app->db->createCommand('TRUNCATE token')->execute();
+    }
+
     public function actionTestEmail(){
         Yii::createObject(Messages::className())->sendMessage(6, Url::to(['/public'], true), true);
     }
 
-	public function actionRegOrg(){
-		foreach(Profile::find()->all() as $profile){
-			if(false == (Organizations::findOne(['user_id' => $profile->user_id]))){
-				try{
-					$this->registerOrganization($profile);
-				}
-				catch(\Exception $e){
-					echo "фуск\n";
-				}
-			}
-		}
-	}
+    public function actionRegOrg(){
+        foreach(Profile::find()->all() as $profile){
+            if(false == (Organizations::findOne(['user_id' => $profile->user_id]))){
+                try{
+                    $this->registerOrganization($profile);
+                }
+                catch(\Exception $e){
+                    echo "фуск\n";
+                }
+            }
+        }
+    }
 
     private function registerOrganization($model){
         $orgAttributes = [
@@ -60,9 +81,9 @@ class HelperController extends Controller
 
     public function actionMemoryMail(){
         $text = 'У вас є запитання без відповіді. ' . '<a href="https://proumstrade.com.ua/questions/answer?id=5956">Переглянути</a>';
-            \Yii::createObject(Messages::className())->sendMessage(13, $text, true);
+        \Yii::createObject(Messages::className())->sendMessage(13, $text, true);
         $text = 'У вас є запитання без відповіді. ' . '<a href="https://proumstrade.com.ua/questions/answer?id=5957">Переглянути</a>';
-            \Yii::createObject(Messages::className())->sendMessage(13, $text, true);
+        \Yii::createObject(Messages::className())->sendMessage(13, $text, true);
     }
 
 }
