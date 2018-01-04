af013b573b393c52ed3c5b810ad65137c9955e5b
mxuser <mxuser@ya.ru>
2018-01-04 11:41:26 +0200

    bugfix

--- views/user/admin/index.php
+++ views/user/admin/index.php
@@ -63,12 +63,16 @@ $dataProvider->sort->defaultOrder = ['confirmed_at' => SORT_ASC, 'created_at' =>
 
 ?>
 
-<div class="panel panel-primary">
-    <div class="panel-heading"><span class="glyphicon glyphicon-stats"></span><strong>
-            <?= Html::encode($this->title); ?></strong></div>
-    <div class="panel-body">
-        <div class="users-view">
-
+<div class="container">
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane auctions fade show active" id="nav-auctions" role="tabpanel" aria-labelledby="nav-auctions-tab">
+            <div class="auctions-header">
+                <div class="row align-items-center">
+                    <div class="col-lg-3">
+                        <h3 class="auctions-title"><?= $this->title; ?></h3>
+                    </div>
+                </div>
+            </div>
             <?= GridView::widget([
                 'dataProvider' 	=> $dataProvider,
                 'filterModel'  	=> $searchModel,
