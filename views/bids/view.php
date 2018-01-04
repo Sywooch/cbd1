893dcb61651e4b4117edaa4d6fcbcb1e3e68a398
mxuser <mxuser@ya.ru>
2018-01-04 21:25:46 +0200

    bugfix

--- views/bids/view.php
+++ views/bids/view.php
@@ -366,7 +366,7 @@ JS;
                                 <p>Посилання для участі</p>
                             </div>
                             <div class="col">
-                                <p><?= Html::a($model->participationUrl, $model->participationUrl, ['target' => '_blank']); ?></p>
+                                <p><?= Html::a($model->participationUrl, $model->participationUrl, ['target' => '_blank', 'auction-url']); ?></p>
                             </div>
                         </div>
                     <?php endif; ?>
