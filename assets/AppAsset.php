35952b8b21ae694c43e89e24df17462298a98a9d
mxuser <mxuser@ya.ru>
2018-01-26 11:48:27 +0200

    bugfix

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,19 +18,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css',
-        'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        'https://cdn.jsdelivr.net/jquery.webui-popover/1.2.1/jquery.webui-popover.min.css',
+        'css/bootstrap.css',
+        'css/font-awesome.css',
+        'css/popover.css',
         'dist/custom.min.css',
         'css/fixes.css'
-
     ];
     public $js = [
-        //'https://code.jquery.com/jquery-3.2.1.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.all.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js',
-        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js',
-        'https://cdn.jsdelivr.net/jquery.webui-popover/1.2.1/jquery.webui-popover.min.js',
+        'js/sweetalert.js',
+        'js/popper.js',
+        'js/bootstrap.js',
+        'js/popover.js',
         'js/accounting.js',
         'dist/scripts.min.js'
 
