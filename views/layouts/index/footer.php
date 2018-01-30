61d7ede479a8e5839a8d5c76c34c4268f0ee0cdc
mxuser <mxuser@ya.ru>
2018-01-30 17:42:26 +0200

    bugfix

--- views/layouts/index/footer.php
+++ views/layouts/index/footer.php
@@ -29,30 +29,30 @@
                 <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-2">
                     <ul class="footer-navigation__list list-unstyled">
                         <li class="footer-navigation__item footer-navigation__item--big">
-                            <a href="http://strateg.react-logic.com/site/about" class="footer-navigation__link text-uppercase">Про нас</a>
+                            <a href="/site/about" class="footer-navigation__link text-uppercase">Про нас</a>
                         </li>
                     </ul>
                 </div>
                 <div class="col-xs-12 col-sm-4 col-md-2">
                     <ul class="footer-navigation__list list-unstyled">
                         <li class="footer-navigation__item footer-navigation__item--big">
-                            <a href="http://strateg.react-logic.com/participant/reglament" class="footer-navigation__link text-uppercase">Учасникам</a>
+                            <a href="/participant/reglament" class="footer-navigation__link text-uppercase">Учасникам</a>
                         </li>
                         <li class="footer-navigation__item">
-                            <a href="http://strateg.react-logic.com/participant/pravo-vymogy" class="footer-navigation__link">Право вимоги</a>
+                            <a href="/participant/pravo-vymogy" class="footer-navigation__link">Право вимоги</a>
                         </li>
                         <li class="footer-navigation__item">
-                            <a href="http://strateg.react-logic.com/participant/maino" class="footer-navigation__link">Майно</a>
+                            <a href="/participant/maino" class="footer-navigation__link">Майно</a>
                         </li>
                         <li class="footer-navigation__item">
-                            <a href="http://strateg.react-logic.com/participant/reglament" class="footer-navigation__link">Регламент</a>
+                            <a href="/participant/reglament" class="footer-navigation__link">Регламент</a>
                         </li>
                     </ul>
                 </div>
                 <div class="col-xs-12 col-sm-4 col-md-2">
                     <ul class="footer-navigation__list list-unstyled">
                         <li class="footer-navigation__item footer-navigation__item--big">
-                            <a href="http://strateg.react-logic.com/site/zamovnykam" class="footer-navigation__link text-uppercase">Замовникам</a>
+                            <a href="/site/zamovnykam" class="footer-navigation__link text-uppercase">Замовникам</a>
                         </li>
                         <!-- <li class="footer-navigation__item">
                             <a href="#" class="footer-navigation__link">Текст</a>
@@ -75,7 +75,7 @@
                 <div class="col-xs-12 col-sm-4 col-md-2">
                     <ul class="footer-navigation__list list-unstyled">
                         <li class="footer-navigation__item footer-navigation__item--big">
-                            <a href="http://strateg.react-logic.com/site/contacts" class="footer-navigation__link text-uppercase">Контакти</a>
+                            <a href="/site/contacts" class="footer-navigation__link text-uppercase">Контакти</a>
                         </li>
                     </ul>
                 </div>
